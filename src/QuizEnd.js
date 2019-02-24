487d6f6b83feb82f49ac7d259d4b0cf5c89563b2
shrisha-h <shrisha.hallale@gmail.com>
2019-02-24 10:46:46 +0530

    Add files via upload

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -4,10 +4,10 @@ class QuizEnd extends Component
 	render()
 	{
 		return (
-	<div>
-        <p>Thanks for playing!</p>
-        <a href=''>Reset Quiz</a>
-      </div>
+		<div>
+		<p>Thanks for playing!</p>
+		<a href=''> Reset Quiz</a>
+		</div>
 	)
 	}
 }
