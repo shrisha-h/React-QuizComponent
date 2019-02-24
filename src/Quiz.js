64ae350e3133c5d0233e4e3953e8fab37df205be
shrisha-h <shrisha.hallale@gmail.com>
2019-02-24 10:19:58 +0530

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,8 +12,7 @@ class Quiz extends  Component{
 
 	render()
 	{
-		const isQuizEnd=false
-		//((this.state.quiz_position-1)===quizData.quiz_questions.length)
+		const isQuizEnd=((this.state.quiz_position-1)===quizData.quiz_questions.length)
 		return(
 
 				<div>
